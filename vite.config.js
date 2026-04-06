c82f8195e819c75538462caf1b47b6b2e3c4879e
AnfalAltalhi <150941932+AnfalAltalhi@users.noreply.github.com>
2026-04-06 20:06:28 +0300

    fix vite conflict

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
 import dotenv from 'dotenv';
 
 dotenv.config()
-=======
-import tailwindcss from '@tailwindcss/vite'
->>>>>>> 4c502574498bac528f760118143b8602b0a717f5
-
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
