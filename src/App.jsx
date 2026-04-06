2859f928d02ffa682af7177495791456e0f00a76
AnfalAltalhi <150941932+AnfalAltalhi@users.noreply.github.com>
2026-04-06 19:52:21 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import { BrowserRouter, Route, Routes,Navigate } from "react-router" 
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import AuthLayout from "./layouts/AuthLayout/AuthLayout";
+import Login from "./Pages/Auth/Login";
+import Register from "./Pages/Register/Register";
+import Guard from "./guards/Guard";
+import AdminLayout from "./layouts/AdminLayout/AdminLayout/AdminLayout";
+import Dashboard from "./layouts/AdminLayout/Dashboard/Dashboard";
+import Home from "./pages/Home"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={} >
-          
-          </Route>
-
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<Navigate to="/" replace />} />
+
+        <Route path="/" element={<Navigate to="/auth" />} />
+
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route index element={<Login />} />
+        </Route>
+
+        <Route path="/register" element={<Register />} />
+
+        <Route element={<Guard />}>
+          <Route path="/dashboard" element={<AdminLayout />}>
+            <Route index element={<Dashboard />} />
+          </Route>
+        </Route>
+
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App
